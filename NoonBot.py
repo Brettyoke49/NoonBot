72f0ecfa62063501432d6a72f5e05ae9dd88c1b5
Brett Probert <brettyoke1@hotmail.com>
2021-10-02 15:15:37 -0400

    Finished V1 of NoonBot. All commands work as they are.

--- NoonBot.py
+++ NoonBot.py
@@ -1,15 +1,19 @@
 # bot.py
 import os
+from dotenv import load_dotenv
+import datetime
+from pytz import timezone
+
 from discord.ext import tasks, commands
 import discord
-from dotenv import load_dotenv
 
 from Roll import Roll
-from Roles import *
+from Roles import Roles
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+ERRORS_CHANNEL = int(os.getenv('ERRORS_CHANNEL'))
 
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 
@@ -18,31 +22,60 @@ async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
     print(f'{bot.user.name} has connected to {guild.name}(id: {guild.id})!')
 
-@bot.command(name = "Roll", help = "[Low] [High] - Roll a number between low and high. Default is 1 through 100")
+#Roll
+@bot.command(name = "Roll", help = "[Low] [High] - Roll a number from low to high. Default is 1-100")
 async def _roll(ctx, low: int=None, high: int=None):
     roller = Roll(ctx.author.mention)
     message = roller.roll(low, high)
     if message != None:
         await ctx.send(message)
 
+#Kick
 @bot.command(name = "Kick", help = "[User] - Kick a given user. Use this wisely")
 async def _kick(ctx, member: discord.Member):
     await ctx.send("Hey everyone! " + ctx.author.mention + " just tried to kick " + member.mention + "! What a bastard!")
 
+#Add Role
 @bot.command(name = "AddRole", help = "[Role] - Add the specified role to yourself")
 async def _addRole(ctx, role: discord.Role):
-    return
+    roleManager = Roles(ctx.author)
+    message = await roleManager.addRole(role)
+    await ctx.send(message)
 
+#Remove Role
 @bot.command(name = "RemoveRole", help = "[Role] - Remove the specified role from yourself")
 async def _removeRole(ctx, role: discord.Role):
-    return
+    roleManager = Roles(ctx.author)
+    message = await roleManager.removeRole(role)
+    await ctx.send(message)
 
+#List Roles
+@bot.command(name = "ListRoles", help = "List all of your current roles")
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def _test(ctx):
+    roleManager = Roles(ctx.author)
+    message = roleManager.listRoles()
+    await ctx.send(message)
+
+#COMMAND ERROR HANDLER
 @bot.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    if isinstance(error, commands.RoleNotFound):
+        await ctx.send("That role doesn't exist, check your capitalization")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have permissions to add that role")
+    elif isinstance(error, commands.MemberNotFound):
+        await ctx.send("That member doesn't exist")
+    elif isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f"This command is on cooldown. Please try again after {round(error.retry_after, 1)} seconds.")
+    elif isinstance(error, commands.UserInputError):
+        await ctx.send("Invalid input for that command")
+    else:
+        est = timezone('US/Eastern')
+        message = str(datetime.datetime.now(est)) + " - message: " + ctx.message.content + "\nError(s):" + ', '.join(error.args)
+        errorChannel = bot.get_channel(ERRORS_CHANNEL)
+        await errorChannel.send(message)
 
 bot.run(TOKEN)
--- Roles.py
+++ Roles.py
@@ -1,17 +1,41 @@
+import os
 from discord.ext import tasks, commands
 import discord
 
 class Roles:
-    member
+    member: discord.Member = None
 
     def __init__(self, member):
         self.member = member
 
-    def addRole(self, role):
-        return "add"
+    async def addRole(self, role):
+        if role in self.member.roles:
+            return f"{self.member.mention} already has role {role}"
+        else:
+            try:
+                await self.member.add_roles(role)
+                return f"Added {role} role to {self.member.mention}"
+            except:
+                return f"Failed to add {role} role to {self.member.mention}"
 
-    def removeRole(self, role):
-        return "remove"
+    async def removeRole(self, role):
+        if role not in self.member.roles:
+            return f"{self.member.mention} does not have role {role}"
+        else:
+            try:
+                await self.member.remove_roles(role)
+                return f"Removed {role} role from {self.member.mention}"
+            except:
+                return f"Failed to remove {role} role from {self.member.mention}."
 
     def listRoles(self):
-        return "list"
+        roles = ""
+        delimiter = ""
+
+        for role in self.member.roles:
+            if role.name == "@everyone": 
+                continue
+            roles += delimiter + role.name
+            delimiter = ", "
+
+        return f"Your roles: {roles}"
